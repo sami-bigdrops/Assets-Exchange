a655b524865ad4a2e70b7f03097830f3fd7d92a0
sami-bigdrops <sami@bigdropsmarketing.com>
2025-11-12 13:25:28 +0530

    Initial Commit

---
+++ apps/admin/app/layout.tsx
@@ -0,0 +1,25 @@
+import { Plus_Jakarta_Sans } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { Providers } from "@/components/providers"
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${fontSans.variable} font-sans antialiased `}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}

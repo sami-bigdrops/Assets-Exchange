838934893b3e1b72e51b9763f6f83120696e148c
Yash BigDrops <yash@bigdrops.com>
2026-02-24 00:06:55 +0530

    feat: merge telegram integration from dev including migrations and notification updates

--- features/notifications/types.ts
+++ features/notifications/types.ts
@@ -1,12 +1,9 @@
 export type WorkflowEvent = {
   event:
     | "request.approved_by_admin"
-    | "request.forwarded_to_advertiser"
     | "request.rejected_by_admin"
-    | "request.sent_back_by_admin"
     | "response.approved_by_advertiser"
-    | "response.sent_back_by_advertiser"
-    | "response.rejected_by_advertiser";
+    | "response.sent_back_by_advertiser";
   requestId: string;
   offerName: string;
   fromStatus: string;
